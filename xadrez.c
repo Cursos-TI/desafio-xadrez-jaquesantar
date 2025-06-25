456546b66a2c03cc73828e6a1694edf6a1075d28
Jaqueline <jaque.santar@gmail.com>
2025-06-24 23:16:44 -0300

    nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
 
+
+void movertorre(int casas){
+    if (casas > 0){
+        printf("Torre moveu %d casas para direita\n", casas);
+        movertorre(casas -1);
+    }
+}
+
+void moverbispo (int casas){
+    if (casas > 0){
+        printf("Bispo moveu %d casas para diagonal\n", casas);
+        moverbispo(casas -1);
+    }
+}
+void moverrainha (int casas){
+    if (casas > 0){
+        printf("Rainha moveu %d casas para esquerda \n", casas);
+        moverrainha(casas -1);
+    }
+}
 int main(){
 
-    // TORRE 5 CASAS PARA DIREITA
+    printf("Movimento torre\n");
 
-        for (int t = 0; t < 5 ; t++){
-            printf ("Torre moveu para direita\n");
-        }
+    movertorre(5);
 
-    // BISPO 5 CASAS DIAGONAL DIREITA
+    printf("\n Movimento bispo\n");
+    
+    moverbispo(5);
 
-        printf("\nBispo\n");
+    printf("\n Movimento rainha\n");
 
-        int b = 1;
+    moverrainha(8);
 
-        do
-        {
-            printf ("Bispo moveu %d para diagonal direita\n", b);
-            b++;
-        } while (b <= 5);
-
-    // RAINHA 8 CASAS ESQUERDA 
-
-        printf ("\nRainha\n");  
-
-        int r = 1;
-            
-        while ( r <= 8)
-        {
-            printf("Rainha moveu %d casas para esquerda!\n",r);
-            r++;
-        }
-
+ 
     // CAVALO 2 casas para cima e 1 casa direita
 
-        printf("\nCavalo\n");
+        printf("\nMovimento Cavalo\n");
 
         int movimentocavalo = 1;
 
